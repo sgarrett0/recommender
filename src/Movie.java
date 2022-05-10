c4091787fe1faf68c957d9ee6b00d076868348f0
Daniel Desmond <df.dezmond9@gmail.com>
2022-05-10 19:50:00 -0400

    Movie is up to date with the main branch version

--- src/Movie.java
+++ src/Movie.java
@@ -1,9 +1,58 @@
 public class Movie {
-
+    private String title;
+    private int actionScore;
+    private int comedyScore;
+    private int dramaScore;
+    private int romanceScore;
+    private int mysteryScore;
+    private int horrorScore;
+    private int sciFiScore;
+    private int docScore;
     
-    public Movie(){
-
+    public Movie(String name, int action, int comedy, int drama, int romance, int mystery, int horror, int sciFi, int documetary){
+        this.title = name;
+        this.actionScore = action;
+        this.comedyScore = comedy;
+        this.dramaScore = drama;
+        this.romanceScore = romance;
+        this.horrorScore = horror;
+        this.sciFiScore = sciFi;
+        this.docScore = documetary;
     }
 
+   public String getTitle(){
+       return this.title;
+   }
 
+   public int getActionScore(){
+       return actionScore;
+   }
+
+   public int getComedyScore(){
+       return comedyScore;
+   }
+
+   public int getDramaScore(){
+       return dramaScore;
+   }
+
+   public int getRomanceScore(){
+       return romanceScore;
+   }
+
+   public int getMysteryScore(){
+       return mysteryScore;
+   }
+   
+   public int getHorrorScore(){
+       return horrorScore;
+   }
+
+   public int getSciFiScore(){
+       return sciFiScore;
+   }
+   public int getDocScore(){
+       return docScore;
+   }
 }
+
