01ef82fd0659921025c14521c2dae7552875e0e0
sgarrett0 <sgarrett@salesianum.org>
2022-05-10 13:26:08 -0400

    added sorter file

---
+++ src/Sorter.java
@@ -0,0 +1,11 @@
+public class Sorter {
+    public static void main(){
+
+    }
+    //iterate list of movies
+    //randomly select categories
+    //split movies in the selected genre by determined pivot point
+    //movies need hash variable to contain the bucket location
+    
+
+}
