413cf6962c9e0981ff1ee87c37156bbea77c9625
Daniel Desmond <df.dezmond9@gmail.com>
2022-05-13 10:22:27 -0400

    LSHed the users

--- src/User.java
+++ src/User.java
@@ -4,9 +4,11 @@ public class User {
     private int comedyScore;
     private int dramaScore;
     private int romanceScore;
+    private int mysteryScore;
     private int horrorScore;
     private int sciFiScore;
     private int docScore;
+    private int hash = 1;
 
     public User(String name, int actionScore, int comedyScore, int dramaScore, int romanceScore, int mysteryScore, int horrorScore, int sciFiScore, int docScore){
         this.name = name;
@@ -14,11 +16,60 @@ public class User {
         this.comedyScore = comedyScore;
         this.dramaScore = dramaScore;
         this.romanceScore = romanceScore;
+        this.mysteryScore = mysteryScore;
         this.horrorScore = horrorScore;
         this.sciFiScore = sciFiScore;
         this.docScore = docScore;
     }
 
+    @Override
+    public String toString() {
+        return "User{ name=" + name +"; hash=" + hash+"}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getActionScore() {
+        return actionScore;
+    }
+
+    public int getComedyScore() {
+        return comedyScore;
+    }
+
+    public int getDramaScore() {
+        return dramaScore;
+    }
+
+    public int getRomanceScore() {
+        return romanceScore;
+    }
+
+    public int getMysteryScore() {
+        return mysteryScore;
+    }
+
+    public int getHorrorScore() {
+        return horrorScore;
+    }
+
+    public int getSciFiScore() {
+        return sciFiScore;
+    }
+
+    public int getDocScore() {
+        return docScore;
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    public void setHash(int finalDigit) {
+        this.hash = this.hash * 10 + finalDigit;
+    }
     
     
     
